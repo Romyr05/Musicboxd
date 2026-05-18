1990f54f914755b80a26314530690397c8a2a74c
romyr <romyrlopez123@gmail.com>
2026-05-18 23:04:11 +0800

    chore: added updated database config for mysql

--- app/config/database.php
+++ app/config/database.php
@@ -1,11 +1,21 @@
 <?php
 
-function db(): PDO{  //php data object (for security and switching database purposes)
-    $databasePath = __DIR__ . '/../../database/database.sqlite';
+function db(): PDO
+{
+    $host = getenv('DB_HOST') ?: '127.0.0.1';
+    $port = getenv('DB_PORT') ?: '3306';   //Default port
+    $name = getenv('DB_DATABASE') ?: 'final_proj';
+    $user = getenv('DB_USERNAME') ?: 'root';
+    $pass = getenv('DB_PASSWORD') ?: '';
 
-    //PDO ERROR HANDLING, CONNECTION AND FETCHING
-    $pdo = new PDO('sqlite' . databasePath);  
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-    $pdo->exec('PRAGMA foreign_key = ON');  //Foreign key on
+    $dsn = "mysql:host={$host};port={$port};dbname={$name};charset=utf8mb4";
+
+    //using PDO documentation
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]);
+
+    return $pdo;
 }
