95084cfb3da2a308bb7d857e064570c8a6c88cb3
romyr <romyrlopez123@gmail.com>
2026-05-19 23:07:00 +0800

    chore: linked bootstrap for the magnifying glass

--- app/views/landing.php
+++ app/views/landing.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Musicboxd</title>
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="assets/css/userHeader.css">
 </head>
 <body>
 
