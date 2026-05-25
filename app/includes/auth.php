c3f72fab05d4e22e8cc1d6b15fe2d6cbabe35d44
romyr <romyrlopez123@gmail.com>
2026-05-25 23:55:14 +0800

    fix: merge issue

--- app/includes/auth.php
+++ app/includes/auth.php
@@ -40,48 +40,6 @@ function loginUser(object $user): void
     $_SESSION['username'] = $user->username;
 }
 
-
-function requireLogin(): void
-{
-    if (!isLoggedIn()) {
-        header('Location: /login.php');
-        exit;
-    }
-}
-
-
-function logoutUser(): void
-{
-    $_SESSION = [];
-    session_destroy();
-}
-<?php
-# helper file only
-
-
-//using php session
-
-
-if(session_status() === PHP_SESSION_NONE){
-    session_start();
-}
-
-require_once __DIR__ . '/../config/database.php';
-
-
-function isLoggedIn(): bool{
-    return isset($_SESSION['user_id']);
-}
-
- 
-function loginUser(object $user): void
-{
-    session_regenerate_id(true);
-
-    $_SESSION['user_id'] = $user->id;
-    $_SESSION['username'] = $user->username;
-}
-
 function currentUsername(): ?string
 {
     return $_SESSION['username'] ?? null;
@@ -91,7 +49,7 @@ function currentUsername(): ?string
 function requireLogin(): void
 {
     if (!isLoggedIn()) {
-        header('Location: login');
+        header('Location: /login');
         exit;
     }
 }
