a68f9d8c63940035de9eeb19c3515dde22bdd02a
Yuan Birondo <ycbirondo@up.edu.ph>
2026-05-28 00:59:46 +0800

    chore: update review seeder

--- database/reviews.sql
+++ database/reviews.sql
@@ -8,12 +8,13 @@ yuan12345
 romyr12345
 angel12345
 jba12345
+crow12345
 */
-    ('Yuan', 'yuan@email.com', '$2y$10$u/cKuA3QWYm3u.nDewpY2OvgMm5TCg0D4N75w0PsxVx5l0ughMxcW'),
-    ('Romyr', 'romyr@email.com', '$2y$10$0tERpyr4tvA8qUdWTq1KBu58OWw/sPr13.8oVvz2ByQn61Cck4AAW'),
-    ('Angel', 'angel@email.com', '$2y$10$GAc1nlJsLUm8ZzPk.zF4Q.m3JWhZnlKfkMmveRhGmC1lW0R0SY4Iq'),
-    ('JB_Aparicio', 'jbaparicio@email.com', '$2y$10$TqK5xISlId763rkGm8WXU.jtIgHVo9uetVFk74Wr5wJlt4Y5I0ca2'),
-    ('Crow', 'crow@email.com', '$2y$10$dDHUw2AfU20a5ZR9T3CAVeCUOCSuKgO4I3WIYkG4zfDQHDjh1M83i');
+    (1, 'Yuan', 'yuan@email.com', '$2y$10$u/cKuA3QWYm3u.nDewpY2OvgMm5TCg0D4N75w0PsxVx5l0ughMxcW'),
+    (2, 'Romyr', 'romyr@email.com', '$2y$10$0tERpyr4tvA8qUdWTq1KBu58OWw/sPr13.8oVvz2ByQn61Cck4AAW'),
+    (3, 'Angel', 'angel@email.com', '$2y$10$GAc1nlJsLUm8ZzPk.zF4Q.m3JWhZnlKfkMmveRhGmC1lW0R0SY4Iq'),
+    (4, 'JB_Aparicio', 'jbaparicio@email.com', '$2y$10$TqK5xISlId763rkGm8WXU.jtIgHVo9uetVFk74Wr5wJlt4Y5I0ca2'),
+    (5, 'Crow', 'crow@email.com', '$2y$10$dDHUw2AfU20a5ZR9T3CAVeCUOCSuKgO4I3WIYkG4zfDQHDjh1M83i');
 
 INSERT IGNORE INTO song_reviews (
     user_id,
@@ -48,8 +49,8 @@ VALUES
     (3, 6, 3, 'I don''t get the hype', 0, 3),
     (4, 6, 1, 'I dislike this song!', 0, 2),
     (2, 6, 1, 'I dislike this song!', 0, 2),
-    (10, 2, 5, 'Lifechanging', 1, 12),
-    (10, 3, 2, 'Could be better...', 0, 5);
+    (5, 2, 5, 'Lifechanging', 1, 12),
+    (5, 3, 2, 'Could be better...', 0, 5);
 
 INSERT IGNORE INTO album_reviews (
     user_id,
@@ -84,5 +85,5 @@ VALUES
     (3, 6, 3, 'I don''t get the hype', 0, 3),
     (4, 6, 1, 'I dislike this album!', 0, 2),
     (2, 6, 1, 'I dislike this album!', 0, 2),
-    (10, 1, 4, 'Brings back memories!', 1, 4),
-    (10, 4, 5, 'A classic banger!!!', 1, 3);
+    (5, 1, 4, 'Brings back memories!', 1, 4),
+    (5, 4, 5, 'A classic banger!!!', 1, 3);
